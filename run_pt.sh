e8cb83cd55f775025e86e1d74271f322dcb232ed
Developer <developer@developermacpro.lan>
2018-11-04 10:57:04 +1100

    fixed path in shell script

--- run_pt.sh
+++ run_pt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh /Users/developer/.jenkins/workspace/performance verification/jmeter5/bin/jmeter.sh -n -t jmeter_testplans/testplan_web.jmx -l jmeter5/log/JTLlog.log -j jmeter5/log/Jmeterlog.log -e -o jmeter5/log/report
+sh /Users/developer/.jenkins/workspace/performance_verification/jmeter5/bin/jmeter.sh -n -t jmeter_testplans/testplan_web.jmx -l jmeter5/log/JTLlog.log -j jmeter5/log/Jmeterlog.log -e -o jmeter5/log/report
